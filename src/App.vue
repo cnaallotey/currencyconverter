093acb7806d836889ec6bbb118a8b8959083f63f
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-01-31 20:28:19 +0000

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import navbar from "../src/components/navbar.vue";
+export default {
+  components: {
+    navbar,
+  },
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <navbar />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,19 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <nav class="w-full bg-slate-900 py-8">
+    <div class="max-w-screen-xl mx-auto flex justify-between items-center">
+      <div>
+        <h2 class="font-extrabold text-xl uppercase text-white">Currency converter</h2>
+      </div>
+      <div class="flex items-center">
+        <img src="../assets/github.svg" class="w-8 h-8 mr-2" alt="github" />
+        <p class="text-base font-medium text-white tracking-normal">View Repo</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>

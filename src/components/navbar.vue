cbae2a547d6194450932cc0e486fad92eee17859
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-02-02 15:01:28 +0000

    adding logo

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,9 +3,10 @@
     <div
       class="max-w-screen-xl mx-auto flex justify-between items-center px-5 md:px-10 xl:px-0"
     >
-      <div>
+      <div class="flex items-center">
+        <img src="../assets/currency.svg" class="w-10 h-10 mr-2" alt="" />
         <h2
-          class="font-extrabold text-xl uppercase text-white cursor-pointer select-none"
+          class="font-extrabold text-lg md:text-xl uppercase text-white cursor-pointer select-none"
         >
           Currency converter
         </h2>
